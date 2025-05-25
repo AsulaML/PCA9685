7224d4f833cf9a5bb7bb3411e8c5fc854f6de726
Michael <michaellambert649@gmail.com>
2025-05-25 18:11:15 +0200

    :clean:

--- PCA9685.h
+++ PCA9685.h
@@ -4,30 +4,29 @@
 #include "I2C2.h"
 
 
-// Register Definitions
-
-#define MODE1 0x00			//Mode  register  1
-#define MODE2 0x01			//Mode  register  2
-#define SUBADR1 0x02		//I2C-bus subaddress 1
-#define SUBADR2 0x03		//I2C-bus subaddress 2
-#define SUBADR3 0x04		//I2C-bus subaddress 3
-#define ALLCALLADR 0x05     //LED All Call I2C-bus address
-#define LED0 0x6			//LED0 start register
-#define LED0_ON_L 0x6		//LED0 output and brightness control byte 0
-#define LED0_ON_H 0x7		//LED0 output and brightness control byte 1
-#define LED0_OFF_L 0x8		//LED0 output and brightness control byte 2
-#define LED0_OFF_H 0x9		//LED0 output and brightness control byte 3
-#define LED_MULTIPLYER 4	// For the other 15 channels
-#define ALLLED_ON_L 0xFA    //load all the LEDn_ON registers, byte 0 (turn 0-7 channels on)
-#define ALLLED_ON_H 0xFB	//load all the LEDn_ON registers, byte 1 (turn 8-15 channels on)
-#define ALLLED_OFF_L 0xFC	//load all the LEDn_OFF registers, byte 0 (turn 0-7 channels off)
-#define ALLLED_OFF_H 0xFD	//load all the LEDn_OFF registers, byte 1 (turn 8-15 channels off)
-#define PRE_SCALE 0xFE		//prescaler for output frequency
-#define CLOCK_FREQ 25000000.0 //25MHz default osc clock
-
 #define PCA9685_ADDR 0b1000000
 
+// Register Definitions
 
+#define PCA9685_REG_ADDR_MODE_1 0x00			// Mode  register  1
+#define PCA9685_REG_ADDR_MODE_2 0x01			// Mode  register  2
+#define PCA9685_REG_ADDR_SUBADR1 0x02		    // I2C-bus subaddress 1
+#define PCA9685_REG_ADDR_SUBADR2 0x03		    // I2C-bus subaddress 2
+#define PCA9685_REG_ADDR_SUBADR3 0x04		    // I2C-bus subaddress 3
+#define PCA9685_REG_ADDR_ALLCALLADR 0x05        // LED All Call I2C-bus address
+#define PCA9685_REG_ADDR_LED0 0x6			    // LED0 start register
+#define PCA9685_REG_ADDR_LED0_ON_L 0x6		    // LED0 output and brightness control byte 0
+#define PCA9685_REG_ADDR_LED0_ON_H 0x7		    // LED0 output and brightness control byte 1
+#define PCA9685_REG_ADDR_LED0_OFF_L 0x8		    // LED0 output and brightness control byte 2
+#define PCA9685_REG_ADDR_LED0_OFF_H 0x9		    // LED0 output and brightness control byte 3
+#define PCA9685_REG_ADDR_ALLLED_ON_L 0xFA       // Load all the LEDn_ON registers, byte 0 (turn 0-7 channels on)
+#define PCA9685_REG_ADDR_ALLLED_ON_H 0xFB	    // Load all the LEDn_ON registers, byte 1 (turn 8-15 channels on)
+#define PCA9685_REG_ADDR_ALLLED_OFF_L 0xFC	    // Load all the LEDn_OFF registers, byte 0 (turn 0-7 channels off)
+#define PCA9685_REG_ADDR_ALLLED_OFF_H 0xFD	    // Load all the LEDn_OFF registers, byte 1 (turn 8-15 channels off)
+#define PCA9685_REG_ADDR_PRE_SCALE 0xFE		    // Prescaler for output frequency
+
+#define CLOCK_FREQ 25000000.0                   //25MHz default osc clock
+#define PCA9685_CH_LED_MULTIPLYER 4	            // For the other 15 channels
 
 
 void PCA9685_Init();
